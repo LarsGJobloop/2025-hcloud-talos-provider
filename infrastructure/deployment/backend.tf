d907c7d101e5fa95ba3b3b34cacf8c82d6fad0ed
zabronax <zabronax@gmail.com>
2025-04-08 19:41:33 +0200

    initiate infrastructure deployment

---
+++ infrastructure/deployment/backend.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    minio = {
+      source  = "aminueza/minio"
+      version = "3.3.0"
+    }
+  }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://hel1.your-objectstorage.com"
+    }
+
+    # Skip AWS configuration, not required by MinIO
+    region                      = "hel1"
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
+    skip_s3_checksum            = true
+  }
+}
